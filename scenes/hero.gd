c9aaf07524d2de8e53bc5cfb1c872ef1380d3677
Ricardo <ricardogomezescalante@gmail.com>
2024-02-13 21:32:18 +0100

    player moves constantly right and camera follows but only horizontally

--- scenes/hero.gd
+++ scenes/hero.gd
@@ -4,7 +4,13 @@ const JUMP_FORCE = 3000
 const GRAVITY: float = 980.0
 const MAX_JUMP_ENERGY: float = 0.3
 
+@export
+var player_speed = 20
+
 var jump_energy: float = 0.0
+
+func _ready() -> void:
+	velocity.x = player_speed
 
 func _process(delta: float) -> void:
 	
@@ -16,5 +22,6 @@ func _process(delta: float) -> void:
 	
 	if Input.is_action_pressed("jump") and jump_energy > 0:
 		velocity.y -= delta*JUMP_FORCE
+	
 	
 	move_and_slide()
